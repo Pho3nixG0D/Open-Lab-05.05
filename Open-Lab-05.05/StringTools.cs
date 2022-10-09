c478c675615ec7d4c3b3b3429e83d0463ca87a61
Pho3nixG0D <oliverkrskagmail.com>
2022-10-09 21:58:54 +0200

    Created for loop check space, and if its second postion set upper or lowercase

--- Open-Lab-05.05/StringTools.cs
+++ Open-Lab-05.05/StringTools.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework.Internal.Execution;
+using System;
+using System.Reflection;
+using System.Text;
 
 namespace Open_Lab_05._05
 {
@@ -6,7 +9,35 @@ namespace Open_Lab_05._05
     {
         public string AlternatingCaps(string original)
         {
-            throw new NotImplementedException();
+            string results = string.Empty;
+            int count = 0;
+
+            for (int i = 0; i < original.Length; i++)
+            {
+                //space check
+                if (original[i] == ' ')
+                {
+                    results += ' ' ;
+                    continue;
+                }
+                //check every second letter set uppercase
+                if (count % 2 == 0)
+                {
+                    results += char.ToUpper(original[i]);
+                    count++;
+                }
+                //set to lowercase
+                else
+                {
+                    count++;
+                    results += char.ToLower(original[i]);
+
+                }
+            }
+            return results;
+
+
         }
+        
     }
 }
